ed74af089c369f2dea23adc8304e74d2764790d6
Paul Murray <pmurray@bigpond.com>
2020-07-13 14:57:15 +1000

    Added favicon

--- webserver.cpp
+++ webserver.cpp
@@ -9,10 +9,13 @@
 #include "webserver.h"
 #include "passwords.h"
 #include "clock.h"
+#include "favicon.h"
 
 ESP8266WebServer server(80); // Create a webserver object that listens for HTTP request on port 80
 
 void handleState();
+void handleRoot();
+void handleFavicon();
 
 void webserver_setup() {
   // Connect to Wi-Fi network with SSID and password
@@ -37,13 +40,12 @@ void webserver_setup() {
 
   Serial.println("Setup done");
 
-  server.on("/", []() {
-    Serial.println("got root");
-    server.send(200, "text/html", "<html><body><a href=\"/state\">State</a></body></html>"); // Send HTTP status 200 (Ok) and send some text to the browser/client
-  });
+  server.on("/", handleRoot);
   server.on("/state", handleState);
+  server.on("/favicon.ico", handleFavicon);
+  server.on("favicon.ico", handleFavicon);
   server.onNotFound([]() {
-    Serial.println("got not root");
+    Serial.println("got not found");
     server.send(404, "text/plain", "404: Not found");
   });
 
@@ -61,10 +63,20 @@ void handleState() {
   rtc.get(dt);
   toJson(dt);
 
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.sendHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  server.sendHeader("Access-Control-Allow-Headers", "*");
-  server.sendHeader("Access-Control-Max-Age", "86400");
+  server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
+  server.sendHeader(F("Access-Control-Allow-Methods"), F("POST, GET, OPTIONS"));
+  server.sendHeader(F("Access-Control-Allow-Headers"), F("*"));
+  server.sendHeader(F("Access-Control-Max-Age"), F("86400"));
 
   server.send(200, "application/json", json); // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleRoot() {
+    Serial.println("got root");
+    server.send(200, F("text/html"), F("<html><body><a href=\"/state\">State</a></body></html>")); // Send HTTP status 200 (Ok) and send some text to the browser/client
+
+}
+
+void handleFavicon() {
+  server.send_P(200, (PGM_P) F("image/x-icon"), (PGM_P) favicon, favicon_len);  
 }
